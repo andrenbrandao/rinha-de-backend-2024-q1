1da4ee3a52f8ad2b612e7a5162a6c2a63e36c52d
André Brandão <andrenbrandao@gmail.com>
2024-03-01 22:35:02 -0300

    refactor: handlers to separate functions

--- main.go
+++ main.go
@@ -32,34 +32,37 @@ func seedDB() {
 	}
 }
 
-func main() {
-	fmt.Println("Starting up server...")
+func healthHandler(w http.ResponseWriter, _ *http.Request) {
+	fmt.Fprintf(w, "Server is running!\n")
+}
 
-	http.HandleFunc("GET /health", func(w http.ResponseWriter, _ *http.Request) {
-		fmt.Fprintf(w, "Server is running!\n")
-	})
+func extratoHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+	fmt.Printf("Retornando extrato do cliente de id %s...\n", id)
+	w.Header().Set("Content-Type", "application/json")
 
-	http.HandleFunc("GET /clientes/{id}/extrato", func(w http.ResponseWriter, r *http.Request) {
-		id := r.PathValue("id")
-		fmt.Printf("Retornando extrato do cliente de id %s...\n", id)
-		w.Header().Set("Content-Type", "application/json")
-
-		responseBody := struct {
-			Saldo struct {
-				Total       int    `json:"total"`
-				DataExtrato string `json:"data_extrato"`
-				Limite      int    `json:"limite"`
-			} `json:"saldo"`
-		}{struct {
+	responseBody := struct {
+		Saldo struct {
 			Total       int    `json:"total"`
 			DataExtrato string `json:"data_extrato"`
 			Limite      int    `json:"limite"`
-		}{Total: -9098, DataExtrato: "2024-01-17T02:34:41.217753Z", Limite: 100000}}
+		} `json:"saldo"`
+	}{struct {
+		Total       int    `json:"total"`
+		DataExtrato string `json:"data_extrato"`
+		Limite      int    `json:"limite"`
+	}{Total: -9098, DataExtrato: "2024-01-17T02:34:41.217753Z", Limite: 100000}}
 
-		b, _ := json.Marshal(responseBody)
-		w.WriteHeader(http.StatusOK)
-		w.Write(b)
-	})
+	b, _ := json.Marshal(responseBody)
+	w.WriteHeader(http.StatusOK)
+	w.Write(b)
+}
+
+func main() {
+	fmt.Println("Starting up server...")
+
+	http.HandleFunc("GET /health", healthHandler)
+	http.HandleFunc("GET /clientes/{id}/extrato", extratoHandler)
 
 	fmt.Println("Listening to requests on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
